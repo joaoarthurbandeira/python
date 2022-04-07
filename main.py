92e9317dd7968f8bd32db44d4a8daf0a92a7ea12
joao arthur bandeira <joaoarthurbandeira@joaos-MacBook-Pro.local>
2022-04-07 07:33:52 -0300

    autenticação senha do usuário e número de tentativas para update no db

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from user import User
 from user_specify import User_specify
 
-profissao = input("Digite a sua profissão: ").lower()
-# usuario = User()
-# usuario.cadastro()
-User_specify().cadastro(profissao)
+while True:  # teste para não ter que ficar digitando de novo
+    profissao = input("Digite a sua profissão: ").lower()
+    # usuario = User()
+    # usuario.cadastro()
+    # esse parametro profissao substituirá "None" no parâmetro "area" do def cadastro
+    User_specify().cadastro(area=profissao)
--- user.py
+++ user.py
@@ -15,14 +15,7 @@ class User():
         db[self.email] = {}  # identificador_unico
         db[self.email]["Nome"] = self.nome
         db[self.email]["Senha"] = self.senha
-        if area != None:
-            db[self.email]["Profissão"] = area
-            if area == "médico":
-                db[self.email]["CRM"] = input("Digite o seu CRM: ")
-            elif area == "advogado":
-                db[self.email]["OAB"] = input("Digite o seu OAB: ")
 
-        # print(db)
         try:
             database = open("db.json", "r")
         # se não tiver encontrado o arquivo, interpretar o que vem abaixo:
@@ -34,10 +27,75 @@ class User():
 
         data = json.load(database)  # dados antigos
 
-        with open("db.json", "r+", encoding="utf8") as json_file:
-            data.update(db)  # dados antigos com dados novos
-            json_file.seek(0)  # define a posição dos dados
-            json.dump(data, json_file, ensure_ascii=False, indent=2)
+        def armazena(self):
+            if area != None:
+                db[self.email]["Profissão"] = area
+                if area == "médico":
+                    db[self.email]["CRM"] = input("Digite o seu CRM: ")
+                elif area == "advogado":
+                    db[self.email]["OAB"] = input("Digite o seu OAB: ")
 
-        # return self.nome, self.email, self.senha
+            # print(db)
+                with open("db.json", "r+", encoding="utf8") as json_file:
+                    data.update(db)  # dados antigos com dados novos
+                    json_file.seek(0)  # define a posição dos dados
+                    json.dump(data, json_file, ensure_ascii=False, indent=2)
+
+        if self.email in data:
+            print("Dados já existentes. Deseja atualizá-los? [S/n]")
+            atualiza = input().lower()
+            if atualiza == "s":
+                # variavel nova para armazenar um dado antigo (senha antiga)
+                senha_cadastrada_db = data[self.email]["Senha"]
+                # se a senha atual (self.senha) for igual à antiga...
+                if self.senha == senha_cadastrada_db:
+                    armazena()
+                else:
+                    for num_tentativa in range(2):
+                        if num_tentativa < 2:
+                            print(
+                                f"senha incorreta!\nDados já existentes. Deseja atualizá-los? [S/n]\nRestam {2 - num_tentativa} tentativas.")
+                            atualiza = input().lower()
+                            if atualiza == "s":
+                                self.senha = input("digite a senha novamente ")
+                                if self.senha == senha_cadastrada_db:
+                                    armazena()  # se ela acertar...
+                                    break
+                            else:  # se ela não quiser atualizar
+                                break
+#
+                        # se ela errar e não tiver excedido, volta para começo do for loop.
+                        if num_tentativa == 1:
+                            # se ela errar e exceder o numero de tentativas
+                            print(
+                                "enviamos um código para redefinição de senha no seu email.")
+
+        # if self.email in data:
+            # print("Dados já existentes. Deseja atualizá-los? [S/n]")
+            # atualiza = input().lower()
+            # if atualiza == "s":
+                # for num_tentativa in range(3):
+                    # if num_tentativa < 3:
+                        # senha_cadastrada_db = data[self.email]["Senha"]
+                        # if self.senha == senha_cadastrada_db:
+                            # armazena()
+                            # break
+                        # else:
+                            # print(
+                            # f"senha incorreta!\nDados já existentes. Deseja atualizá-los? [S/n]\nRestam {3 - num_tentativa} tentativas.")
+                            # atualiza = input().lower()
+                            # if atualiza == "s":
+                            #     if self.senha == senha_cadastrada_db:
+                            #         armazena()
+                            #     break
+                            # else:
+                            #     break
+#
+                # if num_tentativa == 3:
+                    # print(
+                        # "enviamos um código para redefinição de senha no seu email.")
+
+                        #
+#
+        return self.nome, self.email, self.senha
         return print("Cadastro Concluído.")
